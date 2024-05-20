fccf7b0389fae6ee23babc0d4a27b4d1a916d1df
KaziAhosunHabibRipon <habibkazi92@@gmail.com>
2024-05-21 00:15:23 +0600

    initial setup done

---
+++ src/app/server.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "../config";
+
+async function main() {
+  try {
+    await mongoose.connect(config.db_uri as string).then(() => {
+      console.log("Database connected successfully !");
+    });
+    app.listen(config.port, () => {
+      console.log(
+        `Server is connected on port http://localhost:${config.port}`
+      );
+    });
+  } catch (err: any) {
+    console.error("Failed to connect to the database:", err);
+  }
+}
+
+main();
